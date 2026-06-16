39b02e71c1c5084f0122045b7f4176cb2607bca0
CIncin4 <0cincin0x@gmail.com>
2026-06-16 16:52:14 -0300

    BaseJogo-V0.3
    Particulas no movimento
    Ajustes no mapa
    Função para atirar

---
+++ Assets/Projectile_logic.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Projectile_logic : MonoBehaviour
+{
+    [SerializeField] private float bulletSpeed;
+    [SerializeField] private float bulletLifeSpan;
+
+    private void Start(){
+        StartCoroutine(Lifespan());
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision){
+        Destroy(this.gameObject);
+    }
+
+    private void Update(){
+        transform.Translate(0, bulletSpeed * Time.deltaTime, 0);
+    }
+
+    private IEnumerator Lifespan(){
+        yield return new WaitForSeconds(bulletLifeSpan);
+        Destroy(this.gameObject);
+    }
+}
