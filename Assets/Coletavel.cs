df8fc0ed34094444df824fe3c0edd73f2138d5a4
CIncin4 <0cincin0x@gmail.com>
2026-06-18 11:07:14 -0300

    SimonComo-V0.3.2.1
    Lixo no mapa

--- Assets/Coletavel.cs
+++ Assets/Coletavel.cs
@@ -5,6 +5,6 @@ public class Coletavel : MonoBehaviour
     private Rigidbody2D rb;
     void Start(){
         rb = GetComponent<Rigidbody2D>();
-        rb.AddForce(new Vector3(Random.Range(-8, 8), Random.Range(-8, 8), 0));
+        rb.AddForce(new Vector3(Random.Range(-5, 5), Random.Range(-5, 5), 0));
     }
 }
